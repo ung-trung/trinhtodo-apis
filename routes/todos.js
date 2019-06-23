9970614f0cbed95144be8f7617bb93dd9d3efe65
Trung <ungkientrung@gmail.com>
2019-06-23 23:38:29 +0300

    express server setup with initial route

---
+++ routes/todos.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+const User = require('../models/User');
+const Contact = require('../models/Contact');
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
+//Init Middleware
+
+app.use(express.json());
+
+//Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/todos', require('./routes/todos'));
+app.use('/api/auth', require('./routes/auth'));
+
+// // Serve static assets in production
+// if (process.env.NODE_DEV === 'production') {
+//   //set statis folder
+//   app.use(express.static('client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
