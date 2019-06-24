bbb03e88553e57efad47dcbfa091090b6daa9cd2
Trung <ungkientrung@gmail.com>
2019-06-24 22:00:29 +0300

    try deploy backend

--- server.js
+++ server.js
@@ -17,18 +17,8 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/todos', require('./routes/todos'));
 app.use('/api/auth', require('./routes/auth'));
 
-// Serve static assets in production
-if (process.env.NODE_DEV === 'production') {
-  //set statis folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
