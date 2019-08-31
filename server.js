41e115819e4294b12b41c7ff15aca9603d670c4b
Trung <ungkientrung@gmail.com>
2019-08-31 21:17:24 +0300

    why is it not running on Heroku

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 
-const connectDB = require('./config/db');
-const path = require('path');
+const connectDB = require('./config/db')
+const path = require('path')
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT || 1707;
+const PORT = process.env.PORT || 1707
 
 // Connect Database
-connectDB();
+connectDB()
 //Init Middleware
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
 //Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/todos', require('./routes/todos'));
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/users'))
+app.use('/api/todos', require('./routes/todos'))
+app.use('/api/auth', require('./routes/auth'))
 
 app.get('/', function(req, res) {
-  res.send('Welcome to Trinhtodo API');
-});
+  res.send('Welcome to Trinhtodo API')
+})
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+
+// why is it not running
