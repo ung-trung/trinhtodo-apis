15dd6478f7647816c1d35df233afde748215a693
Trung <ungkientrung@gmail.com>
2020-03-14 19:18:42 +0200

    populate field user from backend

--- routes/todos.js
+++ routes/todos.js
@@ -14,6 +14,7 @@ router.get('/', auth, async (req, res) => {
       // @ts-ignore
       .or([{ user: req.user.id }, { subUsers: req.user.id }])
       .populate('subUsers', 'firstName lastName username')
+      .populate('user', 'firstName lastName username')
       .sort({
         date: -1
       })
